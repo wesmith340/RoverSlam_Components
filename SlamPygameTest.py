3574e0cf3e77519551c870345fc633ac651bb359
wesmith340 <wesmith340@gmail.com>
2022-03-31 12:16:53 -0400

    stuff

---
+++ SlamPygameTest.py
@@ -0,0 +1,166 @@
+#!/usr/bin/env python
+from distutils.dir_util import remove_tree
+import math
+from turtle import Vec2D
+import numpy as np
+import roversim
+import random as rand
+import pygame
+import time
+from collections import deque
+
+posResult = []
+oriResult = []
+
+RAD_TO_DEGREES = 180 / math.pi
+PATH_THRESHOLD = 5 # .5 # 50 centimeters
+ONTOP_THRESHOLD = 5
+OFFSET = 300
+
+
+currentPos = None
+
+startPos = None
+targetPos = None
+
+dumpPos = None
+digPos = None
+
+followFlag = False
+recordFlag = False
+
+turnFlag = True
+
+path = deque()
+
+curPath = deque()
+
+def saveHome():
+    # TODO
+    # Implement save dump point and calculate dig point
+    pass
+
+def callback(data):
+    global currentPos, targetPos, turnFlag, recordFlag, followFlag
+    #print(data.poses[-1].position)
+    x = data.poses[-1].position.x
+    y = data.poses[-1].position.y
+    z = data.poses[-1].position.z
+    # print(x*100, y*100, z*100)
+
+    # posResult.append(f'{x},{y},{z}')
+    qX = data.poses[-1].orientation.x
+    qY = data.poses[-1].orientation.y
+    qZ = data.poses[-1].orientation.z
+    w = data.poses[-1].orientation.w
+    
+    
+    # Note still need to normalize quaternion before calculations
+    angTheta = math.acos(w)*2
+    s = math.sqrt(1-w*w) # normalized w < 1 guarenteed
+    if s > 0.001: # avoid division by 0
+        qX = qX / s
+        qY = qY / s
+        qZ = qZ / s
+
+    roversim.ROVER.setPosition(roversim.Vector2(currentPos.y*100, currentPos.x*100))
+    curRad = currentPos.rad
+    if currentPos.qZ < 0:
+        curRad = 2*math.pi - curRad
+    roversim.ROVER.setRotation(curRad)
+
+    roversim.draw()
+    if recordFlag:
+        if len(path) == 0:
+            path.append(currentPos)
+        
+        lastPos = path[-1]
+
+        if math.sqrt((x-lastPos.x)**2+(y-lastPos.y)**2) > PATH_THRESHOLD:
+            roversim.ROVER.addToPath(roversim.Vector2(currentPos.y*100, currentPos.x*100))
+            path.append(currentPos)
+    
+        
+
+def nextMove(currentPos, angle, path):
+    targetPos = path[-1]
+
+    targetAngle = math.atan2(targetPos.y-currentPos.y, targetPos.x-currentPos.x)
+    # curDeg = currentPos.degrees
+    cAng = angle
+    if cAng < 0:
+        cAng = 2*math.pi + cAng
+
+    if targetAngle < 0:
+        targetAngle = 2*math.pi + targetAngle
+    eucDist = math.sqrt((currentPos.x-targetPos.x)**2+(currentPos.y-targetPos.y)**2)
+    if eucDist < ONTOP_THRESHOLD:
+        if(len(path) == 0):
+            # followFlag = False
+            print('END OF PATH')
+        else:
+            targetPos = path.pop()
+            print('STOP')
+            # for point in path:
+            #     print(point.strPos())
+            #     print()
+    elif (math.radians(10) < abs(targetAngle - cAng)):
+        if (targetAngle - cAng < 0):
+            print('TURN LEFT', f'{targetAngle:.2f} {angle:.2f}')
+            angle -= math.radians(5)
+            if angle < -1*math.pi:
+                angle = 2*math.pi + angle
+        else:
+            print('TURN RIGHT', f'{targetAngle:.2f} {angle:.2f}')
+            angle += math.radians(5)
+            if angle > math.pi:
+                angle = -2*math.pi + angle
+    
+    else:
+        x = currentPos.x + 5*math.cos(angle)
+        y = currentPos.y + 5*math.sin(angle)
+        currentPos = roversim.Vector2(x,y)
+        print('FORWARD TO:', eucDist)
+    
+    return currentPos, angle
+
+def randomPoint(point:roversim.Vector2):
+    xOffset = rand.randrange(-1*roversim.WIDTH/2,roversim.WIDTH/2)
+    yOffset = rand.randrange(-1*roversim.HEIGHT/2,roversim.HEIGHT/2)
+    return roversim.ORIGIN + roversim.Vector2(xOffset, yOffset)
+
+if __name__ == '__main__':
+    running = True
+    path = deque()
+
+    roversim.ROVER.setPosition(randomPoint(roversim.ORIGIN))
+    roversim.ROVER.setRotation(math.pi)
+    roversim.draw()
+
+    path.append(roversim.ORIGIN)
+    roversim.ROVER.addToPath(roversim.ORIGIN)
+    
+    for i in range(10):
+        nextPoint = randomPoint(path[-1])
+        print(nextPoint.getTuple())
+        path.append(nextPoint)
+        roversim.ROVER.addToPath(nextPoint)
+    print()
+    for point in path:
+        print(point.getTuple())
+    
+
+    curPos = roversim.ROVER.position
+    angle = roversim.ROVER.rotation
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        roversim.screen.fill((255, 255, 255))
+        roversim.ROVER.draw(roversim.screen)
+        pygame.display.flip()
+
+        curPos, angle = nextMove(curPos, angle, path)
+        roversim.ROVER.setPosition(curPos)
+        roversim.ROVER.setRotation(angle)
+        time.sleep(.1)
---
+++ roversim.py
@@ -0,0 +1,124 @@
+import math
+import pygame
+from typing import List, Tuple
+
+
+class Vector2:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+
+    def getTuple(self) -> Tuple[float, float]:
+        return (self.x, self.y)
+
+    def copy(self) -> object:
+        return Vector2(self.x, self.y)
+
+    def __add__(self, other: object) -> object:
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector2(x, y)
+
+    def __iadd__(self, other: object) -> object:
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector2(x,y)
+
+    def __mul__(self, scalar: float) -> object:
+        self.x *= scalar
+        self.y *= scalar
+        return self
+
+    def __imul__(self, scalar: float) -> object:
+        return self * scalar
+
+
+pygame.init()
+SCALE = .5
+CIRCLE_SIZE = 20
+WIDTH = 900
+HEIGHT = 900
+ORIGIN = Vector2(WIDTH/2, HEIGHT/2)
+screen = pygame.display.set_mode((WIDTH, HEIGHT), 0, 32)
+pygame.display.set_caption("Point2Point Navigation")
+
+
+class Rover:
+    def __init__(self, rotation=0, position: Vector2 = Vector2(0, 0)) -> None:
+        self.rotation = rotation
+        self.position: Vector2 = position
+        self.SIZE = CIRCLE_SIZE
+        self.ROTATIONSTEP = math.pi/1024
+        self.STEP = 0.1
+        self.path: List[Vector2] = []
+
+    def addToPath(self, point: Vector2) -> None:
+        self.path.append(point)
+
+    def setRotation(self, rotation: float) -> None:
+        self.rotation = rotation
+
+    def setPosition(self, position: Vector2) -> None:
+        self.position = position
+
+    def draw(self, screen) -> None:
+        # draw path history
+        for i, point in enumerate(self.path):
+            point = point.copy() + ORIGIN
+            point *= SCALE
+            if i != 0:
+                lastPoint: Vector2 = self.path[i-1].copy() + ORIGIN
+                lastPoint *= SCALE
+                pygame.draw.line(screen, (255, 0, 0),
+                                 lastPoint.getTuple(), point.getTuple())
+            pygame.draw.circle(screen, (0, 255, 0),
+                               point.getTuple(), CIRCLE_SIZE * SCALE)
+
+        # draw rover and rotation
+        rover: Vector2 = self.position.copy() + ORIGIN
+        rover *= SCALE
+        rotVec: Vector2 = Vector2(math.cos(self.rotation), math.sin(self.rotation))
+        rotVec *= self.SIZE
+        rotVec *= SCALE
+        line: Vector2 = rover.copy() + rotVec
+
+        pygame.draw.circle(screen, (0, 0, 255), rover.getTuple(), self.SIZE * SCALE)
+        pygame.draw.line(screen,
+                         (255, 0, 255),
+                         rover.getTuple(),
+                         line.getTuple())
+
+    def turn(self, direction: str) -> None:
+        if direction == 'LEFT':
+            self.rotation += self.ROTATIONSTEP
+        elif direction == 'RIGHT':
+            self.rotation -= self.ROTATIONSTEP
+
+    def move(self, direction: str) -> None:
+        move = Vector2(math.sin(self.rotation), math.cos(self.rotation))
+        move *= self.STEP
+        if direction == 'BACKWARD':
+            move *= -1
+        self.position += move
+
+
+ROVER = Rover()
+
+def draw():
+    screen.fill((255, 255, 255))
+    ROVER.draw(screen)
+    pygame.display.flip()
+
+def quit():
+    pygame.quit()
+
+if __name__ == '__main__':
+    done = False
+    while not done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                done = True
+        screen.fill((255, 255, 255))
+        ROVER.draw(screen)
+        pygame.display.flip()
+
