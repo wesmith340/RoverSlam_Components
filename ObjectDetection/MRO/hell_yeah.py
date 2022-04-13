4e0bf5b2fe96149640d0affe7c4af0e647c898aa
wesmith340 <wesmith340@gmail.com>
2022-04-12 23:04:50 -0400

    Turns out mlsocket doesn't work

--- ObjectDetection/MRO/hell_yeah.py
+++ ObjectDetection/MRO/hell_yeah.py
@@ -13,7 +13,8 @@ from sensor_msgs.msg import Image as rosImage
 from cv_bridge import CvBridge
 
 import threading
-from mlsocket import MLSocket
+# from mlsocket import MLSocket
+import socket
 import pickle
 
 import numpy as np
@@ -34,16 +35,37 @@ IMG_SIZE = 921765
 
 def acceptCon():
     global conList
-    with MLSocket() as s:
+    # with MLSocket() as s:
+    #     while True:
+    #         try:
+    #             s.bind((HOST, PORT))
+    #             s.listen()
+    #             conn, address = s.accept()
+    #             conList.append(conn)
+    #             print('connection from', address)
+    #         except:
+    #             break
+    with socket.socket() as s:
+        s.bind((HOST, PORT))
         while True:
             try:
-                s.bind((HOST, PORT))
                 s.listen()
                 conn, address = s.accept()
                 conList.append(conn)
                 print('connection from', address)
             except:
                 break
+
+def sendImage(image_np):
+    pickleRick = pickle.dumps(image_np)
+    # print(len(pickleRick))
+    for c in conList:
+        try:
+            c.send(pickleRick)
+        except Exception as e:
+            print(e)
+            conList.remove(c)
+            c.close()
 
 t = threading.Thread(target=acceptCon)
 t.start()
@@ -63,23 +85,15 @@ elapsed_time = end_time - start_time
 print('Done! Took {} seconds'.format(elapsed_time))
 
 def callback(data):
-    global imageSize
-
     bridge = CvBridge()
     cv_image = bridge.imgmsg_to_cv2(data, desired_encoding='passthrough')
 
     image_np = np.array(cv_image)
-    for c in conList:
-        try:
-            c.send(image_np)
-        except Exception as e:
-            print(e)
-            conList.remove(c)
-            c.close()
-
-
-
     
+
+    sendImage(image_np) # send image over network
+
+
     input_tensor = tf.convert_to_tensor(np.expand_dims(image_np, 0), dtype=tf.uint8)
     input_tensor = input_tensor[:, :, :, :3]
     detections = detect_fn(input_tensor)
